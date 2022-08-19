a38a8d17c68c847a05cce74be47ba6e01f69ba24
Tom Yuval <tomuvak@gmail.com>
2022-08-19 17:27:20 +0200

    Start working on version 0.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.tomuvak.weak-reference"
-version = "0.0.3"
+version = "0.0.4-SNAPSHOT"
 
 val localProperties = Properties()
 project.rootProject.file("local.properties").takeIf { it.canRead() }?.inputStream()?.let(localProperties::load)
